af580e12e538069e5903617f6034e30543a23c73
zMynx <lior.dux@develeap.com>
2023-08-19 00:29:51 +0300

    prettier to trunk

--- poc/npm-test/test/test.js
+++ poc/npm-test/test/test.js
@@ -1,22 +1,22 @@
 // test/test.js
-const math = require('../math') // Path might vary based on your project structure
-const expect = require('chai').expect
+const math = require("../math"); // Path might vary based on your project structure
+const expect = require("chai").expect;
 
-describe('Math', function () {
-  describe('add', function () {
-    it('should add two numbers', function () {
-      const result = math.add(2, 3)
-      expect(result).to.equal(5)
-    })
+describe("Math", function () {
+  describe("add", function () {
+    it("should add two numbers", function () {
+      const result = math.add(2, 3);
+      expect(result).to.equal(5);
+    });
 
-    it('should handle negative numbers', function () {
-      const result = math.add(-2, 3)
-      expect(result).to.equal(1)
-    })
+    it("should handle negative numbers", function () {
+      const result = math.add(-2, 3);
+      expect(result).to.equal(1);
+    });
 
-    it('should handle zero', function () {
-      const result = math.add(0, 5)
-      expect(result).to.equal(5)
-    })
-  })
-})
+    it("should handle zero", function () {
+      const result = math.add(0, 5);
+      expect(result).to.equal(5);
+    });
+  });
+});
